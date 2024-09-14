a558305d1adc12ddc46f542db48dda0fd45f891d
IkeoluwaAshade <91098490+IkeoluwaAshade@users.noreply.github.com>
2024-09-14 17:42:10 +0100

    Boxification of FAQs

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,4 +1,7 @@
 import PagesHero from "../reusables/PagesHero"
+import femaleDriver from '../assets/post5.jpg'
+import { IoIosHelpCircle } from "react-icons/io";
+
 
 
 const FAQ = () => {
@@ -7,6 +10,25 @@ const FAQ = () => {
       <PagesHero />
 
       <h1>FAQ</h1>
+
+      {/*  ========== Boxification ==========  */}
+
+      <div className="container">
+        <div className="wrapper">
+          <div className="LHS">
+            <div className="left-top">
+              <img src={femaleDriver} alt="" />
+            </div>
+
+            <div className="left-bottom"></div>
+          </div>
+
+          <div className="RHS">
+            <div className="right-top"></div>
+            <div className="right-bottom"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
