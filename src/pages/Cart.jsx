8daecb876d3eab871048ae6a2f7b1c39841086d4
oyinlomo <saheedbarakat24@gmail.com>
2024-09-17 13:36:43 +0100

    home hero

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,17 @@ const Cart = () => {
   return (
     <div>
         <PagesHero />
-
+        <div className=" ">
+      <ul className="list-decimal flex items-center justify-center gap-14 mt-28 text-3xl border-red-400">
+        <li className=" border-red-400">
+          Shopping Cart
+        </li> 
+        <i class="ri-arrow-right-line"></i>
+        <li> Checkout Details</li>
+        <i class="ri-arrow-right-line"></i>
+        <li>Order Complete</li>
+      </ul>
+      </div>
         <h1>Cart</h1>
     </div>
   )
