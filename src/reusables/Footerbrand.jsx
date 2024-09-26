57c807a148764d22ad0b98e55402e0b32ebb0bc8
Damilola <damilolaoni07@gmail.com>
2024-09-26 09:54:40 +0100

    footer mobile completed

--- src/reusables/Footerbrand.jsx
+++ src/reusables/Footerbrand.jsx
@@ -2,12 +2,12 @@ import logo from '../assets/logo-footer.png'
 
 const Footerbrand = () => {
   return (
-    <div className='flex flex-col items-center justify-center p-4 gap-6'>
+    <div className='flex flex-col items-center justify-center p-4 gap-4'>
         <div className="top">
             <img src={logo} alt="" className='w-[20rem]'/>
         </div>
         <div className="bottom">
-            <p className='text-[#a7aba1] italic text-[0.8rem]'>Copyright © 2024 Car Rentals. All Rights Reserved. Powered by LikeMinded Devs</p>
+            <p className='text-[#a7aba1] italic text-[0.8rem] text-center'>Copyright © 2024 Car Rentals. All Rights Reserved. Powered by LikeMinded Devs</p>
         </div>
     </div>
   )
