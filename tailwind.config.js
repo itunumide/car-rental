9506ccf2678aac9bbb1e491bebd96c7948a90612
adelajahalima <adelajahalima@gmail.com>
2024-10-04 12:40:15 +0100

    Remove axios dependency duplicate from package.json file and custom yellow color from tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,7 @@ export default {
   ],
   theme: {
 
-    extend: {
-      colors: {
-        'custom-yellow': {       // This defines a color named `custom-yellow`
-          100: '#fff4cc',       // Lightest shade
-          200: '#ffe999',       // Lighter shade
-          300: '#ffde66',       // Normal, lighter shade
-          400: '#ffd433',       // Base yellow (close to default)
-          500: '#ffca08',       // Original color (#ffca08)
-          600: '#e6b207',       // Slightly darker
-          700: '#cc9a06',       // Darker
-          800: '#b38205',       // Even darker
-          900: '#996a04',       // Darkest shade
-        },
-      }
-      
-    },
+    extend: {},
 
   },
   plugins: [
