45fa362e7412c35b517fcf77f4d258dfcd15fdcc
henrytrust1111 <henrytrust1111@gmail.com>
2024-10-06 01:37:46 +0100

    Henry's first commit2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-<<<<<<< HEAD
     extend: {
       colors: {
+        customYellow: "#ffca08",
         "--clr-primary": "#ffca08",
-        customBlue: 'rgb(87, 132, 186)',
+        customBlue: "rgb(87, 132, 186)",
+        customP: "#aaaaaa",
+        customAccordionColor: "#5b5748",
+        customGray: "#1e1e1e",
+        formBg: "#1C1601",
       },
       transform: {
-        '3d': 'rotateX(180deg) rotateY(180deg)', // Custom 3D rotation
-      },
-=======
-
-    extend: {
-      colors: {
-        customYellow: '#ffca08',
-        customP: '#aaaaaa',
-        customAccordionColor: '#5b5748',
-        customGray: '#1e1e1e',
-        formBg: '#1C1601',
+        "3d": "rotateX(180deg) rotateY(180deg)", // Custom 3D rotation
       },
 
       screens: {
-        '500px': {'max': '500px'},
-      }
->>>>>>> f94bd053c2eb3e0781c570f7427d5d7be0c40c61
+        "500px": { max: "500px" },
+      },
     },
   },
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    require("tailwindcss"),
+    require("autoprefixer"),
     ({ addUtilities }) => {
       addUtilities({
-        '.scrollbar-hidden': {
-          '&::-webkit-scrollbar': {
-            display: 'none'
+        ".scrollbar-hidden": {
+          "&::-webkit-scrollbar": {
+            display: "none",
           },
-          '-ms-overflow-style': 'none',
-          'scrollbar-width': 'none'
-        }
-      })
-    }
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      });
+    },
   ],
-}
+};
