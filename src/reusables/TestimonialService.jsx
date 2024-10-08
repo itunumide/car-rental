e9fd5d1063471bfadf860844d72262afeb845530
Damilola <damilolaoni07@gmail.com>
2024-10-08 05:17:57 +0100

    new revert

--- src/reusables/TestimonialService.jsx
+++ src/reusables/TestimonialService.jsx
@@ -3,7 +3,7 @@
 const TestimonialService = (props) => {
     return (
       <div className="flex justify-center flex-col font-light">
-        <p className="py-2 w-full md:w-[80%] px-3 md:px-12 rounded-3xl text-[0.9rem] md:text-[1.3rem] italic mb-8" style={{backgroundColor: `${props.titlebg}`, color: `${props.titlecolor}`}}>{props.title}</p>
+        <p className="py-2 md:text-left text-center w-full md:w-[60%] px-3 md:px-12 rounded-3xl text-[0.9rem] md:text-[1.3rem] italic mb-8" style={{backgroundColor: `${props.titlebg}`, color: `${props.titlecolor}`}}>{props.title}</p>
         <p
         className="italic text-[1.2rem] md:text-[2.7rem] font-bold pr-4 text-center md:text-left"
         style={{ color: `${props.color}` }}
